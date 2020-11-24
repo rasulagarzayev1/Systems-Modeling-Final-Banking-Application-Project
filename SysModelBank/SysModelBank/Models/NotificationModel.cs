17cec09aa8498e3d4d4a82501b42d5fa0fb90c40
Starrimus <starrimus7@gmail.com>
2020-11-24 15:57:46 +0200

    added notificationing system (#11, #10)

---
+++ SysModelBank/SysModelBank/Models/NotificationModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SysModelBank.Models
+{
+    public enum NotificationType
+    {
+        danger,
+        success,
+        info
+    }
+
+    public class NotificationModel
+    {
+        public NotificationType Type { get; set; }
+        public string Message { get; set; }
+
+        public NotificationModel(NotificationType type, string message)
+        {
+            Type = type;
+            Message = message;
+        }
+    }
+}
