1989974b7fd2d128f8e7bcdc25e6e411cad22c8e
rasulagarzayev1 <agarzayev.rasul@gmail.com>
2020-12-01 21:57:44 +0200

    autheticated user and admin index pages added

--- systemModelingBank/js/main.js
+++ systemModelingBank/js/main.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-    $(".regularTransaction").click(function(){
-        $(".newTransactionDivContent").slideToggle()
-    })
     $(".generateCodeBtn").click(function(){
         $(".generateCodeDivContent").slideToggle()
     })
