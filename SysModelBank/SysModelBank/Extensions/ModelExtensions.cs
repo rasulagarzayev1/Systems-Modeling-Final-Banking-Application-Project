42b0e265c6b7a0237a8782b6d2e002ed8c11c518
Starrimus <starrimus7@gmail.com>
2020-12-12 19:48:34 +0200

    automatic currency conversion in account view model creation

--- SysModelBank/SysModelBank/Extensions/ModelExtensions.cs
+++ SysModelBank/SysModelBank/Extensions/ModelExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SysModelBank.Data.Models;
 using SysModelBank.Data.Models.Identity;
+using SysModelBank.Data.Models.Settings;
 using SysModelBank.Models.Accounts;
 using SysModelBank.Models.Identity;
 using SysModelBank.Models.Settings;
@@ -12,15 +13,15 @@ namespace SysModelBank.Extensions
 {
     public static class ModelExtensions
     {
-        public static AccountModel ToAccountModel(this Account account) =>
+        public static AccountModel ToAccountModel(this Account account, Currency requestedCurrency) =>
             new AccountModel
             {
-                Balance = account.Balance,
+                Balance = account.Balance * requestedCurrency.RateFromEur,
                 Id = account.Id
             };
 
-        public static IEnumerable<AccountModel> ToAccountsListModels(this ICollection<Account> accounts) =>
-            accounts.Select(x => x.ToAccountModel());
+        public static IEnumerable<AccountModel> ToAccountsListModels(this ICollection<Account> accounts, Currency requestedCurrency) =>
+            accounts.Select(x => x.ToAccountModel(requestedCurrency));
 
         public static UserModel ToUserModel(this User user) =>
             new UserModel
@@ -37,7 +38,7 @@ namespace SysModelBank.Extensions
                     Id = user.CurrencyId,
                     Name = user.Currency.Name
                 },
-                Accounts = user.Accounts.ToAccountsListModels()
+                Accounts = user.Accounts.ToAccountsListModels(user.Currency)
             };
     }
 }
